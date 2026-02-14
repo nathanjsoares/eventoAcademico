9b95cf5c7fc86d3c8cf250528c0e2701e1856e51
nathanjsoares <nathansoares@gmail.com>
2026-02-14 15:23:15 -0300

    first commit

---
+++ src/test/java/com/devsuperior/eventoAcademico/EventoAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.eventoAcademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
